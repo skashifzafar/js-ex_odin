d7df0447b8a71c96b0188b2e4b143685b70f63fd
skz <zafar.syedkashif@gmail.com>
2022-11-19 00:28:32 +0530

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+    if(n<0){
+        return "ERROR"
+    }
+    var out=""
+    for (var i=0;i<n;i++){
+        out+=str
+    }
+    return out
 };
 
 // Do not edit below this line
