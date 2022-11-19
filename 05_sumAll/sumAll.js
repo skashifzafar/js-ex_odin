fc5a5f1eb01776d6adb0c0f653bc00b8699a42f2
skz <zafar.syedkashif@gmail.com>
2022-11-19 16:25:52 +0530

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (Number.isInteger(a) && Number.isInteger(b)){
 
+        if (b<0 || a<0){
+            return "ERROR"
+        }
+        if(b>a){
+            return ((b*(b+1)/2)-(a*(a+1)/2)+a)
+        }
+        else{
+            return ((a*(a+1)/2)-(b*(b+1)/2)+b)  
+        }
+    }
+    else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
