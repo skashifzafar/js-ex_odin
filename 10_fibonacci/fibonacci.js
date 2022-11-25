5a9245593fb763d815b6f8db9e83a9ff51678f8c
skz <zafar.syedkashif@gmail.com>
2022-11-26 01:38:55 +0530

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    val=n*1
+    if (!isNaN(val)){
+        if (val>0){
+            var a=1
+            var b=1
+            if (n>2){
+                for (var i=2;i<n;i++){
+                    b=a+b
+                    a=b-a
+                }
+            }
+            return b
+        }
+    }
+    return 'OOPS'
 
 };
 
