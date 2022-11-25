9a3ae6541733a86bfdd065759aba51b3a1391a69
skz <zafar.syedkashif@gmail.com>
2022-11-26 00:33:36 +0530

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  var sum=0
+  if (arr.length>0){
+  for (var x=0;x<arr.length;x++){
+    sum+=arr[x]
+  }
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var mul=1
+  if (arr.length>0){
+  for (var x=0;x<arr.length;x++){
+    mul*=arr[x]
+  }
+  return mul
+  }
+  else {
+    return 0
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  var fct=1
+  if (a>0){
+    fct=a*factorial(a-1)
+  }
+  return fct
 };
 
 // Do not edit below this line
