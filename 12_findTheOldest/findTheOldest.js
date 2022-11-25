2afee7ec909a9c8d5ac09b6292c838247a6b11e7
skz <zafar.syedkashif@gmail.com>
2022-11-26 02:02:49 +0530

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(life) {
+var age=0
+var out
+life.forEach(element => {
+    if (element.hasOwnProperty('yearOfDeath')){
+        if (age<(element.yearOfDeath-element.yearOfBirth)){
+            age=element.yearOfDeath-element.yearOfBirth
+            out=element
+        }
+    }
+    else{
+        var death=new Date().getFullYear()
+        if (age<(death-element.yearOfBirth)){
+            age=death-element.yearOfBirth
+            out=element
+        }
+    }
+});
+return out
 };
 
 // Do not edit below this line
