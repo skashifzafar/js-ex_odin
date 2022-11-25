e7dfc45a66763391f7dc4a729cf60a90684b2114
skz <zafar.syedkashif@gmail.com>
2022-11-26 01:22:07 +0530

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,30 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    var l=0
+    var r=arr.length-1
+    while(l<=r){
+        //console.log(arr)
+        //console.log(l,r)
+        //console.log(arr[l],arr[r])
+        if  ((arr[l].toUpperCase()!=arr[l].toLowerCase()) && (arr[r].toUpperCase()!=arr[r].toLowerCase())) 
+        {
+            if (arr[l].toLowerCase()!=arr[r].toLowerCase()){
+                return false
+            }
+            l+=1
+            r-=1
+
+        }
+        else
+        {
+            if  (arr[l].toUpperCase()==arr[l].toLowerCase()){
+                l+=1
+            }
+            if  (arr[r].toUpperCase()==arr[r].toLowerCase()){
+                r-=1
+            }
+        }
+    }
+    return true
 
 };
 
